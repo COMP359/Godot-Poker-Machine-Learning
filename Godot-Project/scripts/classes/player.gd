fd4675014c7fd2b0d98f82c5838dd7d1eb42522f
Callum Brezden <callumbrezden@gmail.com>
2024-03-31 11:23:10 -0700

    Game Stage Enum and Playing hand function

--- Godot-Project/scripts/classes/player.gd
+++ Godot-Project/scripts/classes/player.gd
@@ -5,14 +5,19 @@ enum PlayerColor {
   BLUE, RED, YELLOW, GREEN
 }
 
+enum Action {
+	FOLD, CHECK, CALL, RAISE
+}
+
 var player_color: PlayerColor
 var is_human_player: bool
 var hand: Hand
 var balance: int
 var bet: int
 var has_folded: bool = false
+var dealer_signal: Signal
 
-func _init(player_selected: PlayerColor, is_human: bool, new_balance: int) -> void:
+func _init(player_selected: PlayerColor, is_human: bool, new_balance: int, dealer_signal: Signal) -> void:
 	"""Initialize a new player with the selected color, type, and balance."""
 	self.player_color = player_selected
 	self.is_human_player = is_human
@@ -20,6 +25,7 @@ func _init(player_selected: PlayerColor, is_human: bool, new_balance: int) -> vo
 	self.hand = Hand.new()
 	self.bet = 0
 	self.has_folded = false
+	self.dealer_signal = dealer_signal
 
 func reset_player_state() -> void:
 	"""Reset the player's hand, bet, and fold status."""
@@ -42,3 +48,37 @@ func bet_amount(amount: int) -> void:
 func get_hand() -> Hand:
 	"""Return the player's hand."""
 	return self.hand
+
+func human_play_hand() -> void:
+	pass
+
+func ai_play_hand() -> void:
+	"""Have the AI play their hand. (This is a placeholder for the AI logic.)"""
+	var action: Action = Action.CHECK
+	var amount: int = 0
+	var hand_value: int = self.hand.ranking.rank
+	var random_factor: float = randf()
+
+	if hand_value >= 8:
+			if random_factor < 0.7:
+					action = Action.RAISE
+					amount = int(hand_value * 10 * random_factor)
+			else:
+					action = Action.CALL
+
+	elif hand_value >= 5:
+			if random_factor < 0.5:
+					action = Action.RAISE
+					amount = int(hand_value * 10 * random_factor)
+			else:
+					action = Action.CALL
+
+	elif hand_value >= 2:
+			if random_factor < 0.3:
+					action = Action.FOLD
+			else:
+					action = Action.CHECK
+	else:
+			action = Action.FOLD
+
+	dealer_signal.emit(self.player_color, action, amount)
