468ef30e3de91ec8da5bce33b2320a598e16b7c6
AndrewWMeier <123119277+AndrewWMeier@users.noreply.github.com>
2024-04-06 11:58:22 -0700

    wins included and excluding ties are now counted seperatly\

--- scripts/util/percentages.gd
+++ scripts/util/percentages.gd
@@ -18,15 +18,15 @@ func _run():
 	#COMMENT OUT ONE OF THE FOLLOWING TO RUN THE FUNCTION
 
 	# Deal turn
-	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
-	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
-	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
-	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
 
 	#Deal flop
-	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
-	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
-	dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
+	# dealer.community_cards.add_card(dealer.deck_of_cards.draw_card())
 
 	#print out player and community cards
 	print("Player 1 Hand: ", dealer.players[0].hand.cards[0].suit, dealer.players[0].hand.cards[0].value," ", dealer.players[0].hand.cards[1].suit, dealer.players[0].hand.cards[1].value)
@@ -35,27 +35,33 @@ func _run():
 	print("Player 4 Hand: ", dealer.players[3].hand.cards[0].suit, dealer.players[3].hand.cards[0].value," ", dealer.players[3].hand.cards[1].suit, dealer.players[3].hand.cards[1].value)
 
 	#COMMENT OUT ONE OF THE FOLLOWING TO RUN THE FUNCTION
-	#print("Community Cards: ", dealer.community_cards.cards[0].suit, dealer.community_cards.cards[0].value," ", dealer.community_cards.cards[1].suit, dealer.community_cards.cards[1].value," ", dealer.community_cards.cards[2].suit, dealer.community_cards.cards[2].value," ", dealer.community_cards.cards[3].suit, dealer.community_cards.cards[3].value)
-	print("Community Cards: ", dealer.community_cards.cards[0].suit, dealer.community_cards.cards[0].value," ", dealer.community_cards.cards[1].suit, dealer.community_cards.cards[1].value," ", dealer.community_cards.cards[2].suit, dealer.community_cards.cards[2].value)
+	print("Community Cards: ", dealer.community_cards.cards[0].suit, dealer.community_cards.cards[0].value," ", dealer.community_cards.cards[1].suit, dealer.community_cards.cards[1].value," ", dealer.community_cards.cards[2].suit, dealer.community_cards.cards[2].value," ", dealer.community_cards.cards[3].suit, dealer.community_cards.cards[3].value)
+	# print("Community Cards: ", dealer.community_cards.cards[0].suit, dealer.community_cards.cards[0].value," ", dealer.community_cards.cards[1].suit, dealer.community_cards.cards[1].value," ", dealer.community_cards.cards[2].suit, dealer.community_cards.cards[2].value)
 
 	print("\n")
 
 	#make sure you have false and true set to the correct values
-	calculate_percentage(dealer,false,true)
+	calculate_percentage(dealer,true,false)
 
 		
 
 func calculate_percentage(dealer,turn,flop):
-	#make dictionary to store the number of wins for each player, only if player has not folded
+	#make dictionary to store the number of wins for each player (includes ties as wins), only if player has not folded
 	var player_wins: Dictionary = {}
 	for player in dealer.players:
 		if not player.has_folded:
 			player_wins[player] = 0.0
 
+	#stores wins for each player where there was only one winner (ties not included as wins), only if player has not folded
+	var distinct_player_wins: Dictionary = {}
+	for player in dealer.players:
+		if not player.has_folded:
+			distinct_player_wins[player] = 0.0
+
+	var tie_count = 0
 	#calculate size of deck
 	var deck_size = dealer.deck_of_cards.deck_of_cards.size()
-	print("Deck size: ",deck_size)
-
+	
 	if turn:
 		#determine who is the winner on every turn and add them to the total
 		for i in range(0,deck_size):
@@ -75,6 +81,13 @@ func calculate_percentage(dealer,turn,flop):
 			#determine the winner
 			var winners = dealer.determine_winner()
 
+			#if there is only 1 winner, increment the distinct player wins
+			if winners.size() <= 1:
+				for winner in winners:
+					distinct_player_wins[winner] += 1
+			else:
+				tie_count += 1
+
 			#print out who won and with what rank
 			# for player in winners:
 			# 	print(player.player_color," wins with ",player.hand.ranking.rank)
@@ -84,11 +97,12 @@ func calculate_percentage(dealer,turn,flop):
 			#remove the turn card from the community cards
 			dealer.community_cards.cards.remove_at(4)
 
-		return(calculate_win_percentage(player_wins, deck_size))
+		return [calculate_win_percentage(player_wins, deck_size), calculate_win_percentage(distinct_player_wins, deck_size-tie_count)]
 
 	elif flop:
 		var combinations = 0
-		var tie_count = 0
+		
+
 		#determine who is the winner on flop and add them to the total
 		for i in range(0,deck_size):
 			for j in range(i+1,deck_size):
@@ -110,40 +124,25 @@ func calculate_percentage(dealer,turn,flop):
 
 				#determine the winner
 				var winners = dealer.determine_winner()
-				if winners.size() > 1:
+
+				#if there is only 1 winner, increment the distinct player wins
+				if winners.size() <= 1:
+					for winner in winners:
+						distinct_player_wins[winner] += 1
+				else:
 					tie_count += 1
-					print("Tie detected. Community cards:")
-					for card in dealer.community_cards.cards:
-						print(card.suit, card.value)
-					print("Players' hands:")
-					for player in winners:
-						print(player.player_color, "'s hand: ", player.hand.cards[0].suit, player.hand.cards[0].value, player.hand.cards[1].suit, player.hand.cards[1].value)
-		
-
-
-				#print out who won and with what rank
-				# for player in winners:
-				# 	print(player.player_color," wins with ",player.hand.ranking.rank)
 
 				for winner in winners:
 					player_wins[winner] += 1
 				#remove the flop cards from the community cards
 				dealer.community_cards.cards.remove_at(3)
 				dealer.community_cards.cards.remove_at(3)
-		print("Number of ties: ", tie_count)
-		return(calculate_win_percentage_flop(player_wins, combinations))
+		return [calculate_win_percentage(player_wins, combinations), calculate_win_percentage(distinct_player_wins, combinations-tie_count)]
 	
 
-func calculate_win_percentage(player_wins, deck_size):
-	# Convert the number of wins to a win percentage
-	for player in player_wins:
-		player_wins[player] = (player_wins[player] / deck_size) *100
-		# player_wins[player] = round(player_wins[player] * 100) / 100
-		
-	print(player_wins)
-	return player_wins
 
-func calculate_win_percentage_flop(player_wins, combinations):
+
+func calculate_win_percentage(player_wins, combinations):
 	var total_percentage = 0
 	# Convert the number of wins to a win percentage
 	for player in player_wins:
